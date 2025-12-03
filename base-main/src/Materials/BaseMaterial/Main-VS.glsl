ba934e87eafdc82857ee29d4501e730e76bfaced
AlexyBeaulieu <beaa6878@usherbrooke.ca>
2025-12-03 13:51:34 -0500

    ptite correction basematerial

--- base-main/src/Materials/BaseMaterial/Main-VS.glsl
+++ base-main/src/Materials/BaseMaterial/Main-VS.glsl
@@ -7,10 +7,12 @@ uniform mat4 Proj;
 uniform float Time;
 uniform float Amplitude; 
 
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
 layout(location = 0) in vec3 Position;
 layout(location = 2) in vec3 Normal;
-
-out vec3 vNormal;
 
 void main()
 {
