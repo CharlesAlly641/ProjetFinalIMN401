1b44569a2a7bba0ba88208352fb5f54781a972f0
CharlesAlly641 <charles.ally04@gmail.com>
2025-12-04 12:44:27 -0500

    Déformation de la sphère

--- base-main/src/Materials/BaseMaterial/Main-FS.glsl
+++ base-main/src/Materials/BaseMaterial/Main-FS.glsl
@@ -1,7 +1,12 @@
 #version 460
-
+in float pic;
 layout(location = 0) out vec4 Color;
 
 void main() {
-    Color = vec4(0.8, 0.8, 0.8, 1.0);
+    // On ajuste la couleur selon si c'est un pic ou non
+    if (pic > 0.5) {
+        Color = vec4(0.87, 0.87, 0.87, 1);
+    } else {
+        Color = vec4(0.9, 0.9, 0.9, 1.0);
+    }
 }
--- base-main/src/Materials/BaseMaterial/Main-VS.glsl
+++ base-main/src/Materials/BaseMaterial/Main-VS.glsl
@@ -8,6 +8,8 @@ uniform float Time;
 uniform float Amplitude; 
 uniform float Frequence;
 
+out float pic;
+
 out gl_PerVertex {
     vec4 gl_Position;
 };
@@ -17,15 +19,23 @@ layout(location = 2) in vec3 Normal;
 
 void main()
 {
+    vec3 displacedPos = Position;
+    pic = 0.0;
 
-    float displacement = Amplitude * cos(Time * Frequence);
+    // Déformation aux sommets qui ont des index "pairs" uniquement
+    if (gl_VertexID % 2 == 0)
+    {
+        float displacement = Amplitude * cos(Time * Frequence);
+        displacedPos += Normal * displacement;
 
-    vec3 displacedPos = Position + Normal * displacement;
+        // On met pic à 1 si le sommet a subit la déformation
+        pic = 1.0;
+    }
 
+    // Mouvement d'oscillation de l'Objet
     float amplitude = 0.5;
-	float vitesse = 0.002;
-	float decalage = amplitude * sin(Time * vitesse);
-
+    float vitesse = 0.002;
+    float decalage = amplitude * sin(Time * vitesse);
 
     gl_Position = Proj * View * Model * vec4(displacedPos + decalage, 1.0);
 }
